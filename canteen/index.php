97f6ecf2f95415f57c0c64c0046328df82eabbb5
Cecilua <65154517+Cecilua@users.noreply.github.com>
2022-09-20 09:02:41 +1200

    Finished Main Page
    still pretty rough around the edges, but Its getting there

---
+++ canteen/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>WGC Canteen</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="header">
+            <h1>HEADER</h1>
+        </div>
+        <div class="navbar">
+            <a href='#' class="active">link</a>
+            <a href='#'>link</a>
+            <a href='#'>link</a>
+        </div>
+        <div class="main">
+            <div class="menu">
+                <div class="menu-top">
+                    <div>Menu - Hide filters</div>
+                    <div>
+                        Sort by:
+                        <select>
+                            <option>Name A-Z</option>
+                            <option>Price Low-High</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="menu-filters">
+                    <select>
+                        <option>Category</option>
+                    </select>
+
+                    <select>
+                        <option>Dietary Requirements</option>
+                    </select>
+                </div>
+            </div>
+            <div class="products">
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product X</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product Y</div>
+                    <div>Product Descriptions</div>
+                </div>
+                <div class="product">
+                    <div class="product-image"></div>
+                    <div>Product Y</div>
+                    <div>Product Descriptions</div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
